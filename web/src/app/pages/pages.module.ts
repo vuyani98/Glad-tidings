7231554e53d8a4e2cd62c256811effd1b375ac54
unknown <nvuyani307@gmail.com>
2025-01-09 16:13:37 +0200

    added resources and support components

--- web/src/app/pages/pages.module.ts
+++ web/src/app/pages/pages.module.ts
@@ -6,12 +6,16 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { LayoutService } from '../layout/layout.service';
 import { PagesService } from './pages.service';
+import { ResourcesComponent } from './resources/resources.component';
+import { SupportComponent } from './support/support.component';
 
 
 @NgModule({
   declarations: [
     LandingPageComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    ResourcesComponent,
+    SupportComponent
   ],
   imports: [
     CommonModule,
