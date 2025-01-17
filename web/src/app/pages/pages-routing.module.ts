8bd3ee003400ab28ca3e53e0116a8fc189fe3cb8
unknown <nvuyani307@gmail.com>
2025-01-17 15:42:05 +0200

    finished the about page

--- web/src/app/pages/pages-routing.module.ts
+++ web/src/app/pages/pages-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { ResourcesComponent } from './resources/resources.component';
 
 const routes: Routes = [
   { path: '', component:  LandingPageComponent},
-
-  { path: 'about', component: AboutUsComponent}
+  { path: 'about', component: AboutUsComponent},
+  { path: 'resources', component: ResourcesComponent}
 ];
 
 @NgModule({
