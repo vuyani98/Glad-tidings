f16eabc98117343b223bee282858f75eb6a31024
unknown <nvuyani307@gmail.com>
2025-01-20 16:04:28 +0200

    finished school page

--- web/src/app/pages/pages-routing.module.ts
+++ web/src/app/pages/pages-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ResourcesComponent } from './resources/resources.component';
+import { RehobothComponent } from './rehoboth/rehoboth.component';
 
 const routes: Routes = [
   { path: '', component:  LandingPageComponent},
   { path: 'about', component: AboutUsComponent},
-  { path: 'resources', component: ResourcesComponent}
+  { path: 'resources', component: ResourcesComponent},
+  { path: 'rehoboth', component: RehobothComponent}
 ];
 
 @NgModule({
