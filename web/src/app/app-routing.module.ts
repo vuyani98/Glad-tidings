06d90da20c588705b1c9bf71f468395ab55b9a1b
unknown <nvuyani307@gmail.com>
2025-01-27 16:59:58 +0200

    changed favicon and some pics

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [{ path: '', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), RouterModule.forRoot(routes, { scrollPositionRestoration : 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
